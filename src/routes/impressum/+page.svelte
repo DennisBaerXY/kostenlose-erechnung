d890051496de9d1c55bbcba16217b7a8b4ecd8d8
Dennis Bär <Dennis-baer@gmx.net>
2025-06-12 12:20:47 +0200

    feat: Add Impressum page with contact and legal information

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,112 @@
+<div>
+	<h1>Impressum</h1>
+
+	<p>
+		Dennis B&auml;r<br />
+		B&auml;r Solutions<br />
+		Schwambstra&szlig;e 7<br />
+		64287 Darmstadt
+	</p>
+
+	<h2>Kontakt</h2>
+	<p>
+		Telefon: +4915234095187<br />
+		E-Mail: info@baer-solutions.com
+	</p>
+
+	<h2>Umsatzsteuer-ID</h2>
+	<p>
+		Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a
+		Umsatzsteuergesetz:<br />
+		DE454372606
+	</p>
+
+	<h2>Wirtschafts&shy;identifikations&shy;nummer</h2>
+	<p>DE454372606</p>
+
+	<h2>Angaben zur Berufs&shy;haftpflicht&shy;versicherung</h2>
+	<p>
+		<strong>Name und Sitz des Versicherers:</strong><br />
+		VHV Allgemeine Versicherung AG<br />
+		VHV-Platz 1<br />
+		30177 Hannover
+	</p>
+	<h2>EU-Streitschlichtung</h2>
+	<p>
+		Die Europ&auml;ische Kommission stellt eine Plattform zur
+		Online-Streitbeilegung (OS) bereit: <a
+			href="https://ec.europa.eu/consumers/odr/"
+			target="_blank"
+			rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a
+		>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+	</p>
+
+	<h2>
+		Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+	</h2>
+	<p>
+		Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor
+		einer Verbraucherschlichtungsstelle teilzunehmen.
+	</p>
+
+	<h2>
+		Zentrale Kontaktstelle nach dem Digital Services Act - DSA (Verordnung (EU)
+		2022/265)
+	</h2>
+	<p>
+		Unsere zentrale Kontaktstelle f&uuml;r Nutzer und Beh&ouml;rden nach Art.
+		11, 12 DSA erreichen Sie wie folgt:
+	</p>
+	<p>E-Mail: info@baer-solutions.com</p>
+	<p>
+		Die für den Kontakt zur Verf&uuml;gung stehenden Sprachen sind: Deutsch,
+		Englisch.
+	</p>
+</div>
+
+<style>
+	h1 {
+		font-size: 2em;
+		margin-bottom: 0.5em;
+	}
+
+	h2 {
+		font-size: 1.5em;
+		margin-top: 1em;
+	}
+
+	p {
+		margin: 0.5em 0;
+	}
+	a {
+		color: blue;
+		text-decoration: underline;
+	}
+
+	a:hover {
+		text-decoration: none;
+	}
+
+	div {
+		max-width: 800px;
+		margin: auto;
+		padding: 1em;
+		font-family: Arial, sans-serif;
+	}
+
+	@media (max-width: 600px) {
+		div {
+			padding: 0.5em;
+		}
+	}
+
+	@media (max-width: 400px) {
+		h1 {
+			font-size: 1.5em;
+		}
+
+		h2 {
+			font-size: 1.2em;
+		}
+	}
+</style>
